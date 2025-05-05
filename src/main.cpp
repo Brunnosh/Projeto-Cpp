cbc3439d4186ecf4ca4824afbf393fa0b825e593
Bruno Savoia <brunotsavoia@gmail.com>
2025-05-04 22:42:32 -0300

    complete refactor starting

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,56 @@
-#include <game.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
 
-int main() {
-    Game game;
-    if (!game.init()) {
+
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit()) {
         return -1;
     }
-    if (!game.setup()) {
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
         return -1;
     }
-    game.loop();
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+    }
+
+
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
